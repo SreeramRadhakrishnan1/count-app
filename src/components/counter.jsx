d68ee6776c3a89b034072fe125fe792274a927b0
Sree Ram <sreeram720@gmail.com>
2020-09-20 15:16:53 +0530

    updated

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    state= {
+        count = 0
+    }
     render() { 
         return <div><h1>button is next</h1><button>click me</button></div>;
     }
