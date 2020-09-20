1765efe8b273aa6654650eb181d098501a79dee4
Sree Ram <sreeram720@gmail.com>
2020-09-20 15:15:24 +0530

    updated

---
+++ src/components/counter.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    render() { 
+        return <div><h1>button is next</h1><button>click me</button></div>;
+    }
+}
+
+
+export default Counter;
